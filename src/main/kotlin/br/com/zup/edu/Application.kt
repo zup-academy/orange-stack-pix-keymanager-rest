a8fd6710be58cd150e21406fccdf7f9110f8a332
Yuri Matheus <yurimatheusb@hotmail.com>
2021-03-05 16:16:45 -0300

    [swagger] documenta a api

--- src/main/kotlin/br/com/zup/edu/Application.kt
+++ src/main/kotlin/br/com/zup/edu/Application.kt
@@ -1,6 +1,17 @@
 package br.com.zup.edu
 
 import io.micronaut.runtime.Micronaut.*
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+
+@OpenAPIDefinition(
+		info = Info(
+				title = "keymanager",
+				version = "1.0"
+		)
+)
+object Aplication
+
 fun main(args: Array<String>) {
 	build()
 	    .args(*args)
